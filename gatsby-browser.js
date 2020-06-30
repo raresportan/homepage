2ba812dbd494d3973d2140d3df0cf2a75b0719a9
Rares Portan <rares.portan@3pillarglobal.com>
2020-06-30 17:11:57 +0300

    Fonts update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,6 @@
-// custom typefaces
-import "typeface-ibm-plex-sans"
-
 import "prismjs/themes/prism.css"
 import "prismjs/plugins/command-line/prism-command-line.css";
 
 import "./src/prism-dark.css"
+import "./src/fonts.css"
 import "./src/styles.css"
