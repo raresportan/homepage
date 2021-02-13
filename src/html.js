e9e865710db9a553975cff3625732d1018811207
Rares Portan <rares.portan@3pillarglobal.com>
2021-02-13 16:55:20 +0200

    Monetization meta

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="monetization" content="$ilp.uphold.com/FzNEikF9pkQJ" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
