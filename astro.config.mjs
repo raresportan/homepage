9400235f7109bd5621a170d5413faaf9fa0b1d3a
raresportan <rportan@gmail.com>
2024-12-14 13:44:25 +0200

    Upgrade Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 import { remarkSocialImage } from './src/lib/remark-social-image.mjs';
-import image from "@astrojs/image";
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
@@ -21,6 +20,6 @@ export default defineConfig({
       remarkPlugins: [remarkSocialImage],
       rehypePlugins: [remarkReadingTime, rehypeSlug, rehypeAutolinkHeadings, [rehypeAutolinkHeadings, {behavior:'append'}]],
     }), 
-    sitemap(),   
-    image()]
+    sitemap()    
+  ]  
 });
